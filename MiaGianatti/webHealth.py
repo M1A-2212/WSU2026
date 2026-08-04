0de11d402543253d6ca6a81b7f2338b4027e164f
M1A-2212 <22127341@student.westernsydney.edu.au>
2026-08-04 14:49:49 +1000

    Fixed webHealth file commit issue

--- MiaGianatti/webHealth.py
+++ MiaGianatti/webHealth.py
@@ -1,10 +1,11 @@
+import json
 import aws_cdk as cdk
 
 def lambda_handler(event, context):
-    print("Hello world")
+    greeting_target = print ("Hello World")
 
 rule = events.Rule(self, "Rule",
-    schedule=events.Schedule.rate(cdk.Duration.minutes(5))
+    schedule=events_.Schedule.rate(cdk.Duration.minutes(5))
 )
 
 # apply_removal_policy(RemovalPolicy.DESTROY)
