c67ab3cb5f8a375003cd8ffd04388858ed28e01a
M1A-2212 <22127341@student.westernsydney.edu.au>
2026-08-05 13:36:05 +1000

    Still working on the hello world. Getting there I think

--- MiaGianatti/webHealth.py
+++ MiaGianatti/webHealth.py
@@ -1,11 +1,20 @@
 import json
-import aws_cdk as cdk
+from aws_cdk import (
+    aws_lambda as _lambda,
+    aws_events as events,
+    aws_events_targets as targets,
+    RemovalPolicy
+)
 
 def lambda_handler(event, context):
-    greeting_target = print ("Hello World")
+    result = helloFunction()
+
+def helloFunction():
+    print ("Hello World")
 
 rule = events.Rule(self, "Rule",
     schedule=events_.Schedule.rate(cdk.Duration.minutes(5))
 )
+#rule.add_target(targets.LambdaFunction())
 
-# apply_removal_policy(RemovalPolicy.DESTROY)
+result.apply_removal_policy(RemovalPolicy.DESTROY)
